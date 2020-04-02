c637c196de9d6668f57e7a2c7823c510658735b0
idam fadilah <idamfadilah.9c@gmail.com>
2020-04-02 08:43:15 +0700

    Add

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="style/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <!-- Font Awesome JS -->
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
@@ -13,7 +14,7 @@
 
     <title>Hello, world!</title>
   </head>
-  <body class="bg-dark">
+  <body >
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand" href="#">Fadilah.id</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,6 +22,7 @@
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
+      <a href="style/style.css"></a>
       <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
       <a class="nav-item nav-link" href="#">Magic</a>
       <a class="nav-item nav-link" href="#">Imagination</a>
@@ -34,10 +36,57 @@
     <li class="breadcrumb-item active" aria-current="page">Data</li>
   </ol>
 </nav>
-    <div style="text-align:center; margin:30px;">
-        <img src="the unbreakable story.png" width="50" height="50">
-        <h3 class="text-light">On progress</h3>
+    <div class="container-satu">
+    <div class="kolom-utama">
+        <h2>Judul artikel</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem quidem facere harum exercitationem libero tempora? Amet voluptate assumenda asperiores commodi praesentium, illo cupiditate dolor impedit.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo temporibus eos nisi dolor dicta, alias, quibusdam necessitatibus ad nesciunt neque veritatis doloremque omnis impedit beatae vel molestiae, excepturi hic? Aliquam quod qui esse suscipit nihil explicabo mollitia harum, dignissimos laboriosam!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit fugit ipsa facere impedit nesciunt dolore numquam ex laborum pariatur porro asperiores, minus vitae quam animi itaque minima aperiam voluptates cum.</p>
     </div>
+    <div class="sidebar-satu">
+    <h3>Sidebar satu</h3>
+    <ul>
+      <li><a href="">Link 1</a></li>
+      <li><a href="">Link 2</a></li>
+      <li><a href="">Link 3</a></li>
+      <li><a href="">Link 4</a></li>
+      <li><a href="">Link 5</a></li>
+    </ul>
+    </div>
+    <div class="sidebar-dua">
+    <h3>Sidebar dua</h3>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde accusamus suscipit quos temporibus eos sapiente, et eius fuga adipisci earum perferendis sunt aperiam dolorum consequatur.</p>
+    </div>
+
+    
+      <!-- <div class="satu">1</div>
+      <div class="dua">2</div>
+      <div class="tiga">3</div>
+      <div class="empat">4</div>
+      <div class="lima">5</div>
+      <div class="satu">1</div>
+      <div class="dua">2</div>
+      <div class="tiga">3</div>
+      <div class="empat">4</div>
+      <div class="lima">5</div>
+     -->
+    </div>
+    <div class="container-dua">
+        <div class="layanan satu">
+          <h4>layanan 1</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, consectetur.</p>
+        </div>
+        <div class="layanan dua">
+        <h4>layanan 2</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto officia, minima qui cum asperiores quam.</p>
+
+        </div>
+        <div class="layanan tiga">
+        <h4>layanan 3</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatibus voluptatem dignissimos maiores alias sequi architecto repellat provident?</p>
+        </div>
+    </div>
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
