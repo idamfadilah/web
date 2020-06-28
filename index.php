5787b9440fec73d849eeab8a84905acb67634c8b
idam fadilah <idamfadilah.9c@gmail.com>
2020-06-28 23:36:19 +0700

    testing

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
 
-    <title>Hello, world!</title>
+    <title>Hay!</title>
   </head>
   <body >
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -23,7 +23,7 @@
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
       <a href="style/style.css"></a>
-      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link active" href="#">Home<span class="sr-only">(current)</span></a>
       <a class="nav-item nav-link" href="#">Magic</a>
       <a class="nav-item nav-link" href="#">Imagination</a>
     </div>
@@ -36,62 +36,3 @@
     <li class="breadcrumb-item active" aria-current="page">Data</li>
   </ol>
 </nav>
-    <div class="container-satu">
-    <div class="kolom-utama">
-        <h2>Judul artikel</h2>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem quidem facere harum exercitationem libero tempora? Amet voluptate assumenda asperiores commodi praesentium, illo cupiditate dolor impedit.</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo temporibus eos nisi dolor dicta, alias, quibusdam necessitatibus ad nesciunt neque veritatis doloremque omnis impedit beatae vel molestiae, excepturi hic? Aliquam quod qui esse suscipit nihil explicabo mollitia harum, dignissimos laboriosam!</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit fugit ipsa facere impedit nesciunt dolore numquam ex laborum pariatur porro asperiores, minus vitae quam animi itaque minima aperiam voluptates cum.</p>
-    </div>
-    <div class="sidebar-satu">
-    <h3>Sidebar satu</h3>
-    <ul>
-      <li><a href="">Link 1</a></li>
-      <li><a href="">Link 2</a></li>
-      <li><a href="">Link 3</a></li>
-      <li><a href="">Link 4</a></li>
-      <li><a href="">Link 5</a></li>
-    </ul>
-    </div>
-    <div class="sidebar-dua">
-    <h3>Sidebar dua</h3>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde accusamus suscipit quos temporibus eos sapiente, et eius fuga adipisci earum perferendis sunt aperiam dolorum consequatur.</p>
-    </div>
-
-    
-      <!-- <div class="satu">1</div>
-      <div class="dua">2</div>
-      <div class="tiga">3</div>
-      <div class="empat">4</div>
-      <div class="lima">5</div>
-      <div class="satu">1</div>
-      <div class="dua">2</div>
-      <div class="tiga">3</div>
-      <div class="empat">4</div>
-      <div class="lima">5</div>
-     -->
-    </div>
-    <div class="container-dua">
-        <div class="layanan satu">
-          <h4>layanan 1</h4>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, consectetur.</p>
-        </div>
-        <div class="layanan dua">
-        <h4>layanan 2</h4>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto officia, minima qui cum asperiores quam.</p>
-
-        </div>
-        <div class="layanan tiga">
-        <h4>layanan 3</h4>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatibus voluptatem dignissimos maiores alias sequi architecto repellat provident?</p>
-        </div>
-    </div>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-  </body>
-</html>
-
